6a027bb2bb66562898e12f533ff1fc99fdaa577e
caffeine96 <maitreydams@gmail.com>
2017-04-26 10:51:16 +0530

    Update EngWordsOnly.py

--- NLTK/EngWordsOnly.py
+++ NLTK/EngWordsOnly.py
@@ -1,18 +1,22 @@
-from nltk.corpus import words,stopwords
+from nltk.corpus import stopwords
+import enchant
 from nltk.tokenize import RegexpTokenizer
 
 stop_words=set(stopwords.words("english"))
 
+train_text="My name is maaru naam Maitrey"
 #Tokenizing the sentence
 tokenizer= RegexpTokenizer(r'\w+')
 words=tokenizer.tokenize(train_text)
-eng_words=words.words()
+d = enchant.Dict("en_US")
 
 filtered_text= []
 
+
 for w in words:
-    if w not in stop_words:
-		if w in eng_words:
-	    	filtered_text.append(w)
+	if w not in stop_words:
+		if d.check(w):
+			filtered_text.append(w)
 
 print(filtered_text)
+
